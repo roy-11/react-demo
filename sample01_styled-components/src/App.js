5b86e7c10e113d1f98957dbd56f7a1ddd02bbec3
roy-11 <63440731+roy-11@users.noreply.github.com>
2022-08-22 22:01:28 +0900

    feat(sample01): add basic styles

--- sample01_styled-components/src/App.js
+++ sample01_styled-components/src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <h2>styled-components</h2>;
+  return (
+    <div style={{ padding: "2rem", backgroundColor: "grey" }}>
+      <h2 className="title">styled-components</h2>
+      <button className="btn">click me</button>
+    </div>
+  );
 }
 
 export default App;
