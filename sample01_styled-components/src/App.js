208de9aef83be22007df4bbf5bdb0c6054e314f3
roy-11 <63440731+roy-11@users.noreply.github.com>
2022-08-23 20:34:44 +0900

    feat(sample01): add components directory

--- sample01_styled-components/src/App.js
+++ sample01_styled-components/src/App.js
@@ -1,22 +1,5 @@
-import styled from "styled-components";
-
-const BasicTitle = styled.h2`
-  text-align: center;
-  text-transform: capitalize;
-`;
-
-const DefaultButton = styled.button`
-  background: #645fff;
-  color: #fff;
-  border: none;
-  border-radius: 0.25rem;
-  cursor: pointer;
-  text-transform: capitalize;
-  padding: 0.25rem;
-  display: block;
-  width: 200px;
-  margin: 1rem auto;
-`;
+import BasicTitle from "./components/BasicTitle";
+import { DefaultButton } from "./components/Button";
 
 function App() {
   return (
---
+++ sample01_styled-components/src/components/BasicTitle.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const BasicTitle = styled.h2`
+  text-align: center;
+  text-transform: capitalize;
+`;
+
+export default BasicTitle;
