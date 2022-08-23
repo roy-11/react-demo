1f3e79fd69f7578ff61817a6bd6e80ac63c8bb8b
roy-11 <63440731+roy-11@users.noreply.github.com>
2022-08-23 15:13:41 +0900

    feat(sample01): add sample inline and global css

--- sample01_styled-components/src/App.js
+++ sample01_styled-components/src/App.js
@@ -3,6 +3,9 @@ function App() {
     <div style={{ padding: "2rem", backgroundColor: "grey" }}>
       <h2 className="title">styled-components</h2>
       <button className="btn">click me</button>
+      <div style={{ padding: "2rem" }}>
+        <h2 className="section-title">Hallo World</h2>
+      </div>
     </div>
   );
 }
