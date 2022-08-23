2b73ac337678db1a66c25ba200f4df1c94336b56
roy-11 <63440731+roy-11@users.noreply.github.com>
2022-08-23 15:17:01 +0900

    feat(sample01): add first styled component

--- sample01_styled-components/src/App.js
+++ sample01_styled-components/src/App.js
@@ -1,11 +1,14 @@
+import styled from "styled-components";
+const BasicTitle = styled.h2`
+  text-align: center;
+  text-transform: capitalize;
+`;
+
 function App() {
   return (
-    <div style={{ padding: "2rem", backgroundColor: "grey" }}>
-      <h2 className="title">styled-components</h2>
+    <div style={{ padding: "2rem" }}>
+      <BasicTitle>Styled Components</BasicTitle>
       <button className="btn">click me</button>
-      <div style={{ padding: "2rem" }}>
-        <h2 className="section-title">Hallo World</h2>
-      </div>
     </div>
   );
 }
