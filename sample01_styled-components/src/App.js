c34e36a87c72c367048c1fe5964368d9b3bd61bc
roy-11 <63440731+roy-11@users.noreply.github.com>
2022-08-24 23:45:41 +0900

    feat(sample01): add simple props to BasicTitle

--- sample01_styled-components/src/App.js
+++ sample01_styled-components/src/App.js
@@ -4,6 +4,7 @@ import { DefaultButton } from "./components/Button";
 function App() {
   return (
     <div style={{ padding: "2rem" }}>
+      <BasicTitle special>Styled Components</BasicTitle>
       <BasicTitle>Styled Components</BasicTitle>
       <DefaultButton>click me</DefaultButton>
     </div>
--- sample01_styled-components/src/components/BasicTitle.js
+++ sample01_styled-components/src/components/BasicTitle.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const BasicTitle = styled.h2`
   text-align: center;
   text-transform: capitalize;
+  color: ${({ special }) => special && "red"};
 `;
 
 export default BasicTitle;
